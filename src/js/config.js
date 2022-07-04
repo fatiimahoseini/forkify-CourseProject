8ebc633a7aad4469180f3c9835dbcb514f462881
fatiimahoseini <fatiimahoseini@gmail.com>
2022-07-04 22:48:13 +0430

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '573a818b-c1ed-4418-aa0e-eb3e55e82831';
+export const MODAL_CLOSE_SEC = 2.5;
